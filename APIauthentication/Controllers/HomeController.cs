701962f8f92e0af631b0b6475417b4be6f7e6ac6
hardi <hardi@HP-ELITEBOOK>
2024-05-27 20:27:08 +0530

    Add project files.

---
+++ APIauthentication/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using AuthenticationAPI.Models;
+using AuthenticationAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AuthenticationAPI.Controllers
+{
+    public class HomeController : ControllerBase
+    {
+        private readonly Services _services;
+
+        public HomeController(Services services)
+        {
+            _services = services;
+        }
+
+
+        [HttpPost("login")]
+        public IActionResult login([FromBody] userLogin userLogin)
+        {
+            return Ok(_services.jwtTokenGeneration(userLogin));
+        }
+    }
+}
---
+++ APIauthentication/Models/userLogin.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthenticationAPI.Models
+{
+    public class userLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
